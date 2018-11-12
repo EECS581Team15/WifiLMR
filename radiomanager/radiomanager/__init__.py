42efeaa561eda2d8af8fc408947c4cbc9551d040
Zane Cersovsky <zane@bread-and-butter.zjcers.com>
2018-11-11 19:45:12 -0600

    #27: Reworked backend startup to facilitate testing

--- radiomanager/radiomanager/__init__.py
+++ radiomanager/radiomanager/__init__.py
@@ -9,10 +9,41 @@ routing.
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from .api import setup_routing
 
-app = Flask(__name__)
-api = Api(app)
-db = SQLAlchemy(app)
 
-setup_routing(api)
+class FlaskExtensions:
+    """
+    """
+    db = SQLAlchemy()
+    api = Api()
+
+    @classmethod
+    def reset(cls):
+        # cls.db = SQLAlchemy()
+        cls.api = Api()
+
+
+class PRODUCTION_CONFIG:
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = "sqlite://db"
+
+
+class TESTING_CONFIG:
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+
+
+def create_app(config_obj=PRODUCTION_CONFIG):
+    from .api import setup_routing
+    app = Flask(__name__)
+    app.config.from_object(config_obj)
+    setup_routing(FlaskExtensions.api)
+    with app.app_context():
+        FlaskExtensions.api.init_app(app)
+        FlaskExtensions.db.init_app(app)
+    return app
+
+
+def init_app(is_testing=False):
+    config_obj = TESTING_CONFIG if is_testing else PRODUCTION_CONFIG
+    return create_app(config_obj)
