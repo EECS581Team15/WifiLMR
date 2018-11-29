ea43051cac2dd3f37e30e5221ca001436dce9a92
jack <jackdweber@gmail.com>
2018-11-29 08:46:00 -0600

    python stuff

--- radioapp/core/mumble.py
+++ radioapp/core/mumble.py
@@ -8,6 +8,14 @@ Adapter for the pymumble library
 from .pymumble.mumble import Mumble
 
 class MumbleAdapter():
+    """Addaptation of pymumble api"""
 
-    initialize(host, user, port=64738, password='', certfile=None, keyfile=None, reconnect=False, tokens=[], debug=False):
-        self.mumble = Mumble(host, user, port, password, certfile, keyfile, reconnect, tokens, debug)
+    def __init__(self):
+        self.mumble = None
+
+    def initialize(self, host, user, port=64738, password='', certfile=None, keyfile=None, reconnect=False, tokens=[], debug=False):
+        """
+        Creates/initializes the Mumble object
+        """
+        if not self.mumble:
+            self.mumble = Mumble(host, user, port, password, certfile, keyfile, reconnect, tokens, debug)
