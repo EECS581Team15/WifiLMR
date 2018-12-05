e4c33f026abe7b3617ac9bf6cf747df8a77d1e2b
John Ying <holylifealchemist@hotmail.com>
2018-12-05 11:40:04 -0600

    removed uncessary ip variable

--- radioapp/zeroconf.py
+++ radioapp/zeroconf.py
@@ -5,7 +5,6 @@ import threading
 class LmrListener(object): #declare this before calling PairingScan
 
     def __init__(self, my_ip):
-        self.ip_address = my_ip
         self.stop_searching = threading.Event()
 
     def add_service(self, zeroconf, type, name):
@@ -19,5 +18,3 @@ def PairingScan(listener, wait_time=2):
         listener.stop_searching.wait(wait_time) #wait before closing connections
     finally:
         zeroconf.close()
-
-    return listener.ip_address
