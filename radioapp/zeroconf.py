398434fca48879f86136f620b0bddd481dadc9fa
John Ying <holylifealchemist@hotmail.com>
2018-12-05 16:42:29 -0600

    pull request suggestions but unclear of information use

--- radioapp/zeroconf.py
+++ radioapp/zeroconf.py
@@ -1,11 +1,13 @@
-from zeroconf import ServiceBrowser, Zeroconf
-import threading
 
+import threading
 
 class LmrListener(object): #declare this before calling PairingScan
 
     def __init__(self):
         self.stop_searching = threading.Event()
+
+    def remove_service(self, zeroconf, type, name):
+        print("Service %s removed" % (name,))
 
     def add_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
@@ -18,3 +20,10 @@ def PairingScan(listener, wait_time=2):
         listener.stop_searching.wait(wait_time) #wait before closing connections
     finally:
         zeroconf.close()
+
+def Start():
+    zeroconf = Zeroconf()
+    ServiceBrowser(zeroconf, "WifiLMR._tcp.local.", listener)
+
+def Stop():
+    zeroconf.close()
