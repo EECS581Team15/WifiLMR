41437e6ddb685f6e4d44a6804f89514e85b2415e
John Ying <holylifealchemist@hotmail.com>
2019-01-30 17:51:09 -0600

    Updates for logger, callbacks, encapsulation, and debugger

--- radioapp/core/service_discovery.py
+++ radioapp/core/service_discovery.py
@@ -10,3 +10,33 @@ multicast DNS (mDNS).
 """
 
 from zeroconf import ServiceBrowser, Zeroconf
+import logging
+
+class BackendDiscovery:
+
+    def __init__(self, callbackadd, callbackremove):
+        self.zeroconf = Zeroconf()
+        self.servicebrowser = None
+        self.listener = LmrListener(callbackadd, callbackremove)
+
+    class LmrListener: #declare this before calling PairingScan
+
+        def __init__(self, callbackadd, callbackremove):
+            self.callbackadd = callbackadd
+            self.callbackremove = callbackremove
+            self.logger = logging.getLogger(__name__)
+
+        def remove_service(self, zeroconf, type, name):
+            self.logger.debug("Service %s removed" % (name,))
+            self.callbackremove(name)
+
+        def add_service(self, zeroconf, type, name):
+            info = zeroconf.get_service_info(type, name)
+            self.logger.debug("Service %s added, service info: %s" % (name, info))
+            self.callbackadd(name, info)
+
+    def start(self):
+        self.servicebrowser = ServiceBrowser(self.zeroconf, "WifiLMR._tcp.local.", self.listener)
+
+    def stop(self):
+        self.zeroconf.close()
