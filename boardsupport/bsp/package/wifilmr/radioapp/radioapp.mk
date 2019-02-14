aa2698c1559c3213f0386ac785b55f41d1d6335d
Zane Cersovsky <zcersovsky@gmail.com>
2019-02-13 19:41:42 -0600

    #39: Modified the radioapp package file to be a "local" package

--- boardsupport/bsp/package/wifilmr/radioapp/radioapp.mk
+++ boardsupport/bsp/package/wifilmr/radioapp/radioapp.mk
@@ -7,20 +7,7 @@
 RADIOAPP_VERSION = 0.1
 
 # Invokes setuptools so that radioapp gets rebuilt
-define RADIOAPP_BUILD_SRC
-	cd $(BR2_EXTERNAL_WiFiLMR_PATH)/../../radioapp && python3 setup.py sdist
-endef
 
-# Hack to create the download directory since there is no real download
-define RADIOAPP_MAKE_DL_DIR
-	mkdir -pv $(BR2_DL_DIR)/radioapp/
-endef
-
-# Remove the .stamp_extracted to force buildroot to use the current version of radioapp
-define RADIOAPP_REMOVE_EXTRACT_STAMP
-	rm -vf $(BASE_DIR)/build/radioapp-$(RADIOAPP_VERSION)/.stamp_downloaded
-	rm -vf $(BASE_DIR)/build/radioapp-$(RADIOAPP_VERSION)/.stamp_extracted
-endef
 
 # Make the init script executable
 define RADIOAPP_MAKE_INIT_SCRIPT_EXEC
@@ -29,7 +16,8 @@ endef
 
 RADIOAPP_PRE_DOWNLOAD_HOOKS += RADIOAPP_BUILD_SRC
 RADIOAPP_POST_INSTALL_TARGET_HOOKS += RADIOAPP_MAKE_INIT_SCRIPT_EXEC
-RADIOAPP_SITE = file://$(BR2_DL_DIR)/../../../radioapp/dist
+RADIOAPP_SITE = $(BR2_DL_DIR)/../../../radioapp
+RADIOAPP_SITE_METHOD = local
 RADIOAPP_SOURCE = radioapp-$(RADIOAPP_VERSION).tar.gz
 RADIOAPP_SETUP_TYPE = setuptools
 
