0064218833279a27d785d2d4c1a74a840cbdb150
Zane Cersovsky <zcersovsky@gmail.com>
2019-02-20 19:17:53 -0600

    #59: Added initial backlight HAL implementation

---
+++ radioapp/radioapp/hal/backlight.py
@@ -0,0 +1,54 @@
+"""
+radioapp.hal.backlight
+======================
+"""
+import logging
+import os
+
+PWM_PREFIX = "/sys/class/pwm/pwmchip0/"
+PERIOD = 100000000 # nanoseconds
+
+class Backlight:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.duty_cycle = None
+        self.saved_level = 100.0
+        try:
+            os.stat(PWM_PREFIX) # Throws an exception if path does not exist
+        except FileNotFoundError:
+            self.logger.error("PWM not found")
+        else:
+            self.logger.debug("PWM found")
+            with open(PWM_PREFIX+"export", 'w') as export:
+                export.write('0')
+            with open(PWM_PREFIX+"period", 'w') as period:
+                period.write(str(PERIOD))
+            with open(PWM_PREFIX+"enable", 'w') as enable:
+                enable.write('1')
+            self.duty_cycle = open(PWM_PREFIX+"duty_cycle")
+    def set_level(self, percentage):
+        """
+        Sets the backlight to a percentage [0-100]
+        """
+        if not (0 <= percentage <= 100):
+            self.logger.error("Attempting to set backlight to illegal value %s", percentage)
+            return
+        self.saved_level = float(percentage)
+        self.__set_level(self.saved_level)
+    def on(self):
+        """
+        Sets the backlight to last set level
+        """
+        self.__set_level(self.saved_level)
+    def off(self):
+        """
+        Turns the backlight off
+        """
+        self.__set_level(0.0)
+    def __set_level(self, percentage):
+        if self.duty_cycle is None:
+            self.logger.info("Cannot set backlight to %s (device not available)", percentage)
+            return
+        duty = int((percentage/100.0) * PERIOD)
+        self.duty_cycle.write(str(duty))
+        self.duty_cycle.seek(0, 0)
