818b7ddf3dec5ab989e0a339f2409e934ad4a571
Zane Cersovsky <zcersovsky@gmail.com>
2019-02-27 14:02:52 -0600

    #59: Added hack to main.py to set backlight to 100%

--- radioapp/radioapp/main.py
+++ radioapp/radioapp/main.py
@@ -2,6 +2,10 @@
 Entry point for radioApp
 """
 from ui import ui_manager
+from hal import backlight
 if __name__ == "__main__":
+    # Remove the backlight hardcoding once we've got proper UI support/state saving
+    bklight = backlight.Backlight()
+    bklight.set_level(100)
     manager = ui_manager.UIManager()
     manager.main_loop()
