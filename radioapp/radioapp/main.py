3493fc51a5c6c366cde00cf3ee3b525ea4d6480f
Zane Cersovsky <zcersovsky@gmail.com>
2019-03-06 18:58:35 -0600

    #19: Fixed merge of main.py

--- radioapp/radioapp/main.py
+++ radioapp/radioapp/main.py
@@ -7,9 +7,8 @@ from hal import backlight
 
 if __name__ == "__main__":
     hal = HAL()
-    manager = ui_manager.UIManager(hal)
     # Remove the backlight hardcoding once we've got proper UI support/state saving
     bklight = backlight.Backlight()
     bklight.set_level(100)
-    manager = ui_manager.UIManager()
+    manager = ui_manager.UIManager(hal)
     manager.main_loop()
