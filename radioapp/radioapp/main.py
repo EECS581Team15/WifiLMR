5b3c45ec408f835aed543c8e163541572a68fb12
Zane Cersovsky <zcersovsky@gmail.com>
2019-04-20 20:24:11 -0500

    Disabled X11 key rollover while running

--- radioapp/radioapp/main.py
+++ radioapp/radioapp/main.py
@@ -1,11 +1,15 @@
 """
 Entry point for radioApp
 """
+import subprocess
+import atexit
 from ui import ui_manager
 from hal import HAL
 from hal import backlight
 
 if __name__ == "__main__":
+    subprocess.run(["xset", "r", "off"]) # disable key rollover
+    atexit.register(lambda: subprocess.run(["xset", "r", "on"])) # only here to not goof up your dev machine
     hal = HAL()
     # Remove the backlight hardcoding once we've got proper UI support/state saving
     hal.backlight.set_level(100)
