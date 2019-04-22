d30b35c2b2de9514bc97f8b2838172a8246950ce
Zane Cersovsky <zcersovsky@gmail.com>
2019-04-21 20:59:09 -0500

    Updated setup.py to pick up wav files for sound fx

--- radioapp/setup.py
+++ radioapp/setup.py
@@ -5,8 +5,17 @@ setuptools.setup(
     name="radioapp",
     author="EECS 581 Team 15",
     version="0.1",
+    install_requires=open("requirements.txt").read(),
+    dependency_links=[
+        "git+https://github.com/azlux/pymumble.git",
+    ],
     url="https://github.com/EECS581Team15/WifiLMR/",
     packages=setuptools.find_packages(),
+    package_data={
+        "": [
+            "*.wav",
+        ],
+    },
     data_files=[
         ("{}/target/etc/init.d/"
          .format(os.environ["BASE_DIR"]),
