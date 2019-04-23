a0e60e7686b44b9b217a7096bfd62dae3b23b71b
Zane Cersovsky <zcersovsky@gmail.com>
2019-04-23 09:33:15 -0500

    Forced /dev/urandom to think it has enough entropy to start

--- radioapp/radioapp/main.py
+++ radioapp/radioapp/main.py
@@ -3,12 +3,19 @@ Entry point for radioApp
 """
 import subprocess
 import atexit
+import fcntl
 from ui import ui_manager
 from hal import HAL
 from hal import backlight
 from core import Core
 
+def force_random_entropy():
+    RNDADDTOENTCNT = 0x40045201 # Divined by S C I E N C E
+    rand = open("/dev/urandom", 'w')
+    fcntl.ioctl(rand.fileno(), RNDADDTOENTCNT, (1024).to_bytes(4, "little", signed=False))
+
 if __name__ == "__main__":
+    force_random_entropy()
     subprocess.run(["xset", "r", "off"]) # disable key rollover
     atexit.register(lambda: subprocess.run(["xset", "r", "on"])) # only here to not goof up your dev machine
     hal = HAL()
